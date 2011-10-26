4ed3525189a46dfe93077386a63b85555d9cd7cf
Luiz Branco <artfikan@gmail.com>
2011-10-26 10:46:43 -0200

    changed msg logic

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,13 +9,15 @@ class MessagesController < ApplicationController
     @message = Message.new(params[:message])
     @message.user = current_user
     if @message.text.empty?
-      @message.text = "tudo"
+      message = "Ligou o Foda-se"
+    else
+      message = "Ligou o Foda-se para #{@message.text}"
     end
     if @message.save
       current_user.profile.feed!(
-      :picture => "http://foda-se.heroku.com/assets/thumb.png",
+      :picture => "#{request.protocol}#{request.host_with_port}#{asset_path("thumb.png")}",
       :link => fb_canvas_url,
-      :name => "Ligou o Foda-se para #{@message.text}",
+      :name => message,
       :caption => "Ligue voc&#xea; tamb&#xe9;m e seja mais feliz!",
       #:actions => [:name => "Ligar o Foda-se", :link => fb_canvas_url]
     )
